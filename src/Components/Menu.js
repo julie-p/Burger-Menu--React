cb9708a38f0f7e1eda34eb73d38dbab5cbf45766
Julie P <paupertjulie@gmail.com>
2020-08-06 20:07:48 +0200

    refactoring & ajouts de commentaires

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -6,7 +6,6 @@ function Menu() {
 
     let menuToggler = "menu-toggler";
     let topNav = "top-nav";
-
     switch (isOpen) {
         case true:
         menuToggler += " open";
@@ -19,42 +18,43 @@ function Menu() {
     };
 
     return (
-        <div className="App">
+
+      <header>
     
-          <header>
-    
-            <div className={menuToggler} isOpen={isOpen} onClick={()=>setIsOpen(!isOpen)}>
-                <div className="bar half start"></div>
-                <div className="bar"></div>
-                <div className="bar half end"></div>
-            </div>
-    
-              <nav className={topNav}>
-                {/* <ul className="nav-list">
-                      <li>
-                        <Link className="nav-link"></Link>
-                      </li>
-                      <li>
-                        <Link className="nav-link"></Link>
-                      </li>
-                      <li>
-                        <Link className="nav-link"></Link>
-                      </li>
-                      <li>
-                        <Link className="nav-link"></Link>
-                      </li>
-                      <li>
-                        <Link className="nav-link"></Link>
-                      </li>
-                      <li>
-                        <Link className="nav-link"></Link>
-                      </li>
-                    </ul> */}
-              </nav>
-    
-          </header>
-    
+        <div className={menuToggler} isOpen={isOpen} onClick={()=>setIsOpen(!isOpen)}>
+          <div className="bar half start"></div>
+          <div className="bar"></div>
+          <div className="bar half end"></div>
         </div>
+    
+        <nav className={topNav}>
+          {/* <ul className="nav-list">
+            <li>
+              <Link className="nav-link">Accueil</Link>
+            </li>
+            <li>
+              <Link className="nav-link">La Story</Link>
+            </li>
+            <li>
+              <Link className="nav-link">Bons Plans</Link>
+            </li>
+            <li>
+              <Link className="nav-link">Mes Coups de Coeur</Link>
+            </li>
+            <li>
+              <Link className="nav-link">Mon Profil</Link>
+            </li>
+            <li>
+              <Link className="nav-link">Mes Paramètres</Link>
+            </li>
+            <li>
+              <Link className="nav-link">Déconnexion</Link>
+            </li>
+          </ul> */}
+        </nav>
+    
+      </header>
+  
     )
 };
 
